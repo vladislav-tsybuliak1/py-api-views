4185db10d45545678d049ce8335083cd63b2fd6f
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-09-27 15:10:44 +0300

    Add models and migration file

--- cinema/models.py
+++ cinema/models.py
@@ -4,7 +4,38 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    actors = models.ManyToManyField(to="Actor", related_name="movies")
+    genres = models.ManyToManyField(to="Genre", related_name="movies")
     duration = models.IntegerField()
 
-    def __str__(self):
+
+    def __str__(self) -> str:
         return self.title
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=23, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=63)
+    rows = models.PositiveIntegerField()
+    seats_in_row = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return self.name
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
